a1a4ef7f8b905586c4e8710a02f2b94512c1cf18
Vitalii Chura <vcvitaly@yahoo.com>
2023-06-11 12:49:27 +0300

    implement parseShows with Either

--- src/main/scala/ch6/TvShowParser.scala
+++ src/main/scala/ch6/TvShowParser.scala
@@ -8,6 +8,14 @@ package ch6
  * @author Vitalii Chura
  */
 class TvShowParser(attributesParser: TvShowAttributesParser) {
+  
+  def parseShows(rawShows: List[String]): Either[String, List[TvShow]] = {
+    val initialResult: Either[String, List[TvShow]] = Right(List.empty)
+    
+    rawShows
+      .map(parseShow)
+      .foldLeft(initialResult)(addOrResign)
+  }
 
   def parseShow(rawShow: String): Either[String, TvShow] = {
     for {
@@ -15,5 +23,14 @@ class TvShowParser(attributesParser: TvShowAttributesParser) {
       yearStart <- attributesParser.extractYearStart(rawShow).orElse(attributesParser.extractSingleYear(rawShow))
       yearEnd <- attributesParser.extractYearEnd(rawShow).orElse(Right(yearStart))
     } yield TvShow(name, yearStart, yearEnd)
+  }
+
+  private def addOrResign(
+                   parsedShows: Either[String, List[TvShow]],
+                   newParsedShow: Either[String, TvShow]): Either[String, List[TvShow]] = {
+    for {
+      shows <- parsedShows
+      parsedShow <- newParsedShow
+    } yield shows.appended(parsedShow)
   }
 }
--- src/test/scala/ch6/TvShowParserTest.scala
+++ src/test/scala/ch6/TvShowParserTest.scala
@@ -17,24 +17,42 @@ class TvShowParserTest extends AnyFlatSpec with EitherValues with should.Matcher
 
   "The parseShow" should "return Left for an incorrect input string" in {
     val params = Table(
-      ("input", "expectedRes"),
+      ("rawShow", "expectedRes"),
       ("The Wire (-)", "Can't extract single year from The Wire (-)"),
       ("The Wire (oops)", "Can't parse oops"),
       ("(2002-2008)", "Can't extract name from (2002-2008)")
     )
-    forAll(params) { (input: String, expectedRes: String) =>
-      tvShowParser.parseShow(input).left.value should be (expectedRes)
+    forAll(params) { (rawShow: String, expectedRes: String) =>
+      tvShowParser.parseShow(rawShow).left.value should be (expectedRes)
     }
   }
 
-  "The parseShow" should "return a Right(TvShow) for a correct input string " in {
+  it should "return a Right(TvShow) for a correct input string " in {
     val params = Table(
-      ("input", "expectedRes"),
+      ("rawShow", "expectedRes"),
       ("The Wire (2002-2008)", TvShow("The Wire", 2002, 2008)),
       ("Chernobyl (2019)", TvShow("Chernobyl", 2019, 2019)),
     )
-    forAll(params) { (input: String, expectedRes: TvShow) =>
-      tvShowParser.parseShow(input).value should be (expectedRes)
+    forAll(params) { (rawShow: String, expectedRes: TvShow) =>
+      tvShowParser.parseShow(rawShow).value should be (expectedRes)
     }
+  }
+
+  "The parseShows" should "return Left for a list of raw shows given that one of them is incorrectly formatted" in {
+    val params = Table(
+      ("rawShows", "expectedRes"),
+      (List("The Wire (2002-2008)", "[2019]"), "Can't extract name from [2019]"),
+      (List("The Wire (-)", "Chernobyl (2019)"), "Can't extract single year from The Wire (-)")
+    )
+    forAll(params) { (rawShows: List[String], expectedRes: String) =>
+      tvShowParser.parseShows(rawShows).left.value should be(expectedRes)
+    }
+  }
+
+  it should "return Right(List[TvShow]) for a list of correct raw shows" in {
+    val rawShows = List("The Wire (2002-2008)", "Chernobyl (2019)")
+    val resEither = tvShowParser.parseShows(rawShows)
+    val expectedRes = List(TvShow("The Wire", 2002, 2008), TvShow("Chernobyl", 2019, 2019))
+    resEither.value should be (expectedRes)
   }
 }
