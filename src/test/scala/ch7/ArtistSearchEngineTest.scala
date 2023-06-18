2c1afd9f5560dd065943b12e3c694a666d51ff7c
Vitalii Chura <vcvitaly@yahoo.com>
2023-06-18 14:38:52 +0300

    refactor the test - get rid of a Table notation

--- src/test/scala/ch7/ArtistSearchEngineTest.scala
+++ src/test/scala/ch7/ArtistSearchEngineTest.scala
@@ -2,85 +2,89 @@ package io.github.vcvitaly.grokkingfp
 package ch7
 
 import ch7.model.MusicGenre.{HardRock, HeavyMetal, Pop}
+import ch7.model.SearchCondition.{SearchByActiveYears, SearchByGenre, SearchByOrigin}
 import ch7.model.YearsActive.{ActiveBetween, StillActive}
-import ch7.model.{Artist, Location}
+import ch7.model.{Artist, Location, SearchCondition}
 
-import io.github.vcvitaly.grokkingfp.ch7.model.SearchCondition.{SearchByActiveYears, SearchByGenre, SearchByOrigin}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 /**
  * ArtistSearchEngineTest.
  *
  * @author Vitalii Chura
  */
-class ArtistSearchEngineTest extends AnyFlatSpec with should.Matchers with ScalaCheckPropertyChecks {
+class ArtistSearchEngineTest extends AnyFlatSpec with should.Matchers {
 
   val artistSearchEngine: ArtistSearchEngine = ArtistSearchEngine()
 
-  "The searchArtists" should "return the list of artists accordingly to search criteria" in {
-    val artists = List(
+  val artists: List[Artist] = List(
+    Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
+    Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
+    Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
+  )
+
+  "The searchArtists" should "search by genre, origin, active years" in {
+    val searchConditions = List(SearchByGenre(List(Pop)), SearchByOrigin(List(Location("England"))), SearchByActiveYears(1950, 2022))
+    val result = List(Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003)))
+
+    artistSearchEngine.searchArtists(artists, searchConditions) should be (result)
+  }
+
+  it should "search by origin, active years" in {
+    val searchConditions = List(SearchByOrigin(List(Location("England"))), SearchByActiveYears(1950, 2022))
+    val result = List(
+      Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
+      Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
+    )
+
+    artistSearchEngine.searchArtists(artists, searchConditions) should be (result)
+  }
+
+  it should "search by active years 1" in {
+    val searchConditions = List(SearchByActiveYears(1950, 1979))
+    val result = List(
+      Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
+      Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
+    )
+
+    artistSearchEngine.searchArtists(artists, searchConditions) should be (result)
+  }
+
+  it should "search by active years 2" in {
+    val searchConditions = List(SearchByActiveYears(1981, 1984))
+    val result = List(
+      Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
+      Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
+    )
+
+    artistSearchEngine.searchArtists(artists, searchConditions) should be(result)
+  }
+
+  it should "search by genre and active years" in {
+    val searchConditions = List(SearchByGenre(List(HeavyMetal)), SearchByActiveYears(2019, 2022))
+    val result = List(
+      Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
+    )
+
+    artistSearchEngine.searchArtists(artists, searchConditions) should be(result)
+  }
+
+  it should "search by origin and active years" in {
+    val searchConditions = List(SearchByOrigin(List(Location("U.S."))), SearchByActiveYears(1950, 1959))
+    val result = List.empty
+
+    artistSearchEngine.searchArtists(artists, searchConditions) should be(result)
+  }
+
+  it should "search by empty condition list" in {
+    val searchConditions = List.empty
+    val result = List(
       Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
       Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
       Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
     )
-    val params = Table(
-      ("artists", "searchConditions", "result"),
-      (
-        artists,
-        List(SearchByGenre(List(Pop)), SearchByOrigin(List(Location("England"))), SearchByActiveYears(1950, 2022)),
-        List(Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003)))
-      ),
-      (
-        artists,
-        List(SearchByOrigin(List(Location("England"))), SearchByActiveYears(1950, 2022)),
-        List(
-          Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
-          Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
-        )
-      ),
-      (
-        artists,
-        List(SearchByActiveYears(1950, 1979)),
-        List(
-          Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
-          Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
-        )
-      ),
-      (
-        artists,
-        List(SearchByActiveYears(1981, 1984)),
-        List(
-          Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
-          Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
-        )
-      ),
-      (
-        artists,
-        List(SearchByGenre(List(HeavyMetal)), SearchByActiveYears(2019, 2022)),
-        List(
-          Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
-        )
-      ),
-      (
-        artists,
-        List(SearchByOrigin(List(Location("U.S."))), SearchByActiveYears(1950, 1959)),
-        List.empty
-      ),
-      (
-        artists,
-        List.empty,
-        List(
-          Artist("Metallica", HeavyMetal, Location("U.S."), StillActive(1981)),
-          Artist("Led Zeppelin", HardRock, Location("England"), ActiveBetween(1968, 1980)),
-          Artist("Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003))
-        )
-      )
-    )
 
-    forAll(params) { (artists, searchConditions, result) =>
-      artistSearchEngine.searchArtists(artists, searchConditions) should be (result)
-    }
+    artistSearchEngine.searchArtists(artists, searchConditions) should be(result)
   }
 }
